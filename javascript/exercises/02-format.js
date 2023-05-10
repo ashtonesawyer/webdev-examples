139cf68858f3c696ff875bad8e88306af75d5dd6
ashtonesawyer <ashtonesawyer@gmail.com>
2023-05-10 13:41:38 -0700

    js exercises 01-04

--- javascript/exercises/02-format.js
+++ javascript/exercises/02-format.js
@@ -3,8 +3,12 @@ const formatItems = (input) => {
 
   // for reference, see the MDN Number page:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
+  let ret = [];
+  for (const num of input) {
+    ret.push(num.toFixed(3));
+  }
 
-  return input;
+  return ret.join(", ");
 };
 
 console.log(formatItems([1, 4, 17, 26, 41]));
--- javascript/exercises/04-dom.js
+++ javascript/exercises/04-dom.js
@@ -1,17 +1,33 @@
 // Exercise #1:
 // when the user clicks the 'copy' button, copy the user input to the output area
+let copy = document.querySelector("#copy");
+let input = document.querySelector("#userInput1");
+let output = document.querySelector("#output");
 
-// fetch JavaScript objects representing specific elements in the DOM
+copy.addEventListener("click", copyHandler);
+function copyHandler(event) {
+  output.textContent = input.value;
+}
 
-// add an event listener on the target element
+// OR
 
-// callback function to handle event
+// copy.onclick = (event) => {
+//   let text = input.value;
+//   output.textContent = text;
+// };
 
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let input2 = document.querySelector("#userInput2");
 
-// add an event listener on the target element
+let element = document.createElement("div");
+element.setAttribute("class", "output");
+document.querySelector("#inputEventExample").append(element);
 
-// callback function to handle event
+input2.addEventListener("input", inputHandler);
+
+function inputHandler() {
+  element.textContent = input2.value;
+}
