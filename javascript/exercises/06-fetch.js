173dd770f1d8aa0aee7ca8e393ea634a1dafd542
Ashton Sawyer <ashtonesawyer@gmail.com>
2023-05-16 10:53:02 -0700

    js 06 done

--- javascript/exercises/06-fetch.js
+++ javascript/exercises/06-fetch.js
@@ -1,9 +1,47 @@
-const url = 'https://anapioficeandfire.com/api/books/';
+const url = "https://anapioficeandfire.com/api/books/";
 
-const app = document.querySelector('#books');
+const app = document.querySelector("#books");
+const loading = document.querySelector("#loading");
+
+const addBook = (book) => {
+  console.log(book);
+  let container = document.createElement("div");
+  let title = document.createElement("h4");
+  let author = document.createElement("p");
+  let pubYear = document.createElement("p");
+  let pgs = document.createElement("p");
+
+  title.textContent = book.name;
+  author.textContent = `By ${book.authors[0]}`;
+  pubYear.textContent = book.released.substr(0, 4);
+  pgs.textContent = `${book.numberOfPages} pages`;
+
+  container.append(title);
+  container.append(author);
+  container.append(pubYear);
+  container.append(pgs);
+  app.append(container);
+};
 
 const fetchData = (url) => {
-  // Fetch all books from the API of Ice and Fire and append them to the DOM
-  // Create an element for each book that contains title, author, publication year, and number of pages
-  // Update the styles in JavaScript to center all the books in the container given
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      // check the format of the data
+      // if data is in array format, we can use an array method such as forEach()
+      data.forEach((item) => {
+        addBook(item);
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      let div = document.createElement("div");
+      div.textContent = `An error occurred. Please try again.`;
+      app.append(div);
+    })
+    .finally(() => {
+      app.removeChild(loading);
+    });
 };
+
+fetchData(url);
